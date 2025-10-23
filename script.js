533b08fffaa1f5619aabca2185cc214bffee9134
Adriano Lopes <77409420+AGLSANTOS@users.noreply.github.com>
2025-10-23 22:45:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+/*
+  Desenvolvedor: Adriano Gomes Lopes dos Santos
+  Email: adriano.glopes@yahoo.com.br
+  Copyright (c) 2025
+*/
+
+const form = document.getElementById('youtubeForm');
+const googleScriptURL = "https://script.google.com/macros/s/AKfycbx3DMlaNH7-I4WaLWZGjTv-oWFk7XSmd6wMEXblbC3L1ZUsEsxupQokLoDNxbB5p4Va/exec";
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const nome = document.getElementById('nome').value.trim();
+  const link = document.getElementById('link').value.trim();
+  const dataHora = new Date().toLocaleString('pt-PT');
+
+  fetch(googleScriptURL, {
+    method: 'POST',
+    body: JSON.stringify({ nome, link, dataHora }),
+    headers: { 'Content-Type': 'application/json' }
+  })
+  .then(res => res.json())
+  .then(data => {
+    alert("✅ Formulário enviado com sucesso!");
+    form.reset();
+  })
+  .catch(err => alert("❌ Erro ao enviar: " + err));
+});
+
+new QRCode(document.getElementById("qrcode"), {
+  text: window.location.href,
+  width: 128,
+  height: 128
+});
